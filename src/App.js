25d0ed52408a123599e643a3b8d11efa34d82d1c
dolgol <107240677+dolgol@users.noreply.github.com>
2023-09-01 10:24:11 +0900

    [Style] 결제 페이지 링크 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 
 import './styles/index.scss';
@@ -8,12 +8,14 @@ import Inventory from './pages/Inventory';
 import Employees from './pages/Employees';
 import Payment from './pages/Payment';
 import './styles/layout/FullScreenDiv.css'
+import ProductList from 'pages/ProductList';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+          <Route path='/payment' element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
