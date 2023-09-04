fa9b8d66048b4f5a93c6b6a36899a6f068dc9e12
dolgol <107240677+dolgol@users.noreply.github.com>
2023-09-04 13:33:16 +0900

    [Add] 상품 promotion info 함수 생성

--- src/store/utils/function.js
+++ src/store/utils/function.js
@@ -1,3 +1,22 @@
 export const addComma = (price) => {
   return price.toLocaleString();
 };
+
+export const promotion = (promotionInfo) => {
+
+  switch (promotionInfo) {
+    case 1:
+      return "";
+    case 2:
+      return "세일";
+    case 3:
+      return "덤증정";
+    case 4:
+      return "1+1";
+    case 5:
+      return "2+1";
+    default:
+      break;
+  }
+
+}
