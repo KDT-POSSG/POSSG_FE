7f87009b76894d8ac762e49e49f76e09473e4503
정재원 <gpffhwodnjs@gmail.com>
2023-09-08 10:42:25 +0900

    [Fix] 직원 추가 모달 배치 보완

--- src/components/employees/AddEmployeeModal.jsx
+++ src/components/employees/AddEmployeeModal.jsx
@@ -42,6 +42,7 @@ function AddEmployeeModal({onAdd}) {
       .catch(error => {
         // 데이터 전송에 실패했을 경우 실행될 코드
         console.log('데이터 전송 실패', error);
+
       });
   };
 
@@ -51,8 +52,8 @@ function AddEmployeeModal({onAdd}) {
                 
                     <div className="form-row">
                         <div className="input-container">
-                            <input className="input-text" onChange={(e) => setEmpName(e.target.value)} placeholder='이름' required />
-                            <label className="label-helper"><span class="material-symbols-rounded">person</span></label>
+                            <input className="input-text" onChange={(e) => setEmpName(e.target.value)} required />
+                            <label className="label-helper" htmlFor="id"><span>이름</span></label>
                             
                         </div>
                     </div>
@@ -60,38 +61,51 @@ function AddEmployeeModal({onAdd}) {
                     <div className="form-row">
                         <div className="input-container">
                             <input type="text" className="input-text" onChange={(e) => setConvSeq(parseInt(e.target.value))} required />
-                            <label className="label-helper"><span>지점 번호</span></label>
+                            <label className="label-helper" htmlFor="pw"><span>지점 번호</span></label>
                            
                         </div>
                     </div>
                     
                     <div className="form-row">
                       <div className='input-birth-container'>
+
                         <div className="input-container">
-                            <input type="text" className="input-text" onChange={(e) => setBirthDate(e.target.value)} required />
-                            <label className="label-helper"><span>생년월일 (8자리 입력)</span></label>
+                            <input type="text" className="input-birth" onChange={(e) => setBirthDate(e.target.value)} required />
+                            <label className="label-helper" htmlFor="branchName"><span>생년월일 (8자리 입력)</span></label>
                         </div>
-                        <div className="input-gender">
-                          <input type="radio" vlaue="남성" onChange={(e) => setGender(e.target.value)} checked={gender === "남성"}/>
+
+                        {/* <div className="input-gender">
+                          <input type="radio" id="gender-male" vlaue="남성" onChange={(e) => setGender(e.target.value)} checked={gender === "남성"}/>
                           <label htmlFor="gender-male">남성</label>
-                          <input type="radio" vlaue="여성" onChange={(e) => setGender(e.target.value)} checked={gender === "여성"} />
+                          <input type="radio" id="gender-female" vlaue="여성" onChange={(e) => setGender(e.target.value)} checked={gender === "여성"} />
+                          <label htmlFor="gender-female">여성</label>
+                        </div> */}
+
+                        <div className="input-gender">
+                          <input id="gender-male" type="radio" name="gender" value="남성" onChange={(e) => setGender(e.target.value)} checked={gender === "남성"} />
+                          <label htmlFor="gender-male">남성</label>
+                          <input id="gender-female" type="radio" name="gender" value="여성"onChange={(e) => setGender(e.target.value)} checked={gender === "여성"}/>
                           <label htmlFor="gender-female">여성</label>
                         </div>
-                      </div>
-                    </div>
 
+                        </div>
+
+
+                       
+          
+
+                    </div>
                     <div className="form-row">
                         <div className="input-container">
                             <input type="text" className="input-text" onChange={(e) => setPhoneNumber(e.target.value)} required />
-                            <label className="label-helper"><span>연락처 ex.01056852833</span></label>
+                            <label className="label-helper" htmlFor="repreName"><span>연락처 ex.01056852833</span></label>
                             
                         </div>
                     </div>
-
                     <div className="form-row">
                         <div className="input-container">
                             <input type="text" className="input-text" onChange={(e) => setHireDate(e.target.value)} required />
-                            <label className="label-helper"><span>고용 날짜 (8자리 입력)</span></label>
+                            <label className="label-helper" htmlFor="repreName"><span>고용 날짜 (8자리 입력)</span></label>
                             
                         </div>
                     </div>
@@ -99,7 +113,7 @@ function AddEmployeeModal({onAdd}) {
                     <div className="form-row">
                         <div className="input-container">
                             <input type="text" className="input-text" onChange={(e) => setSalary(parseInt(e.target.value))} required />
-                            <label className="label-helper"><span>시급</span></label>
+                            <label className="label-helper" htmlFor="repreName"><span>시급</span></label>
                             
                         </div>
                     </div>
@@ -114,8 +128,145 @@ function AddEmployeeModal({onAdd}) {
                             {/* <input className="regi-btn" type="submit" value="회원가입" disabled={!formIsValid} /> */}
                         </div>
                 
-            </div>
-            </div>
+            </div></div>
+
+    
+      // <div className="container">
+      //   <form>
+      //     <div className="row">
+      //       <h4>Account</h4>
+      //       <div className="input-group input-group-icon">
+      //         <input type="text" placeholder="Full Name" />
+      //         <div className="input-icon"></div>
+      //       </div>
+      //       <div className="input-group input-group-icon">
+      //         <input type="email" placeholder="Email Adress" />
+      //         <div className="input-icon"></div>
+      //       </div>
+      //       <div className="input-group input-group-icon">
+      //         <input type="password" placeholder="Password" />
+      //         <div className="input-icon"></div>
+      //       </div>
+      //     </div>
+      //     <div className="row">
+      //       <div className="col-half">
+      //         <h4>Date of Birth</h4>
+      //         <div className="input-group">
+      //           <div className="col-third">
+      //             <input type="text" placeholder="DD" />
+      //           </div>
+      //           <div className="col-third">
+      //             <input type="text" placeholder="MM" />
+      //           </div>
+      //           <div className="col-third">
+      //             <input type="text" placeholder="YYYY" />
+      //           </div>
+      //         </div>
+      //       </div>
+      //       <div className="col-half">
+      //         <h4>Gender</h4>
+      //         <div className="input-group">
+      //           <input id="gender-male" type="radio" name="gender" value="male" />
+      //           <label htmlFor="gender-male">Male</label>
+      //           <input id="gender-female" type="radio" name="gender" value="female" />
+      //           <label htmlFor="gender-female">Female</label>
+
+      //<label>
+    //         <input 
+    //           type="radio" 
+    //           value="남성" 
+    //           checked={gender === "남성"} 
+    //           onChange={(e) => setGender(e.target.value)}
+    //         />
+    //         남성
+    //       </label>
+    //       <label>
+    //         <input 
+    //           type="radio" 
+    //           value="여성" 
+    //           checked={gender === "여성"} 
+    //           onChange={(e) => setGender(e.target.value)}
+    //         />
+    //         여성
+    //       </label>
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <div className="row">
+      //       <h4>Payment Details</h4>
+      //       <div className="input-group">
+      //         <input id="payment-method-card" type="radio" name="payment-method" value="card" defaultChecked />
+      //         <label htmlFor="payment-method-card"><span>Credit Card</span></label>
+      //         <input id="payment-method-paypal" type="radio" name="payment-method" value="paypal" />
+      //         <label htmlFor="payment-method-paypal"><span>Paypal</span></label>
+      //       </div>
+      //       <div className="input-group input-group-icon">
+      //         <input type="text" placeholder="Card Number" />
+      //         <div className="input-icon"></div>
+      //       </div>
+      //       <div className="col-half">
+      //         <div className="input-group input-group-icon">
+      //           <input type="text" placeholder="Card CVC" />
+      //           <div className="input-icon"></div>
+      //         </div>
+      //       </div>
+      //       <div className="col-half">
+      //         <div className="input-group">
+      //           <select>
+      //             <option>01 Jan</option>
+      //             <option>02 Jan</option>
+      //           </select>
+      //           <select>
+      //             <option>2015</option>
+      //             <option>2016</option>
+      //           </select>
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <div className="row">
+      //       <h4>Terms and Conditions</h4>
+      //       <div className="input-group">
+      //         <input id="terms" type="checkbox" />
+      //         <label htmlFor="terms">I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</label>
+      //       </div>
+      //     </div>
+      //   </form>
+      // </div>
+    
+    // <div className='addemployeemodal'>
+    //   <h1>직원 모달</h1>
+    //   <div className='addemp-container'>
+    //   <form>
+    //     <input type="text" placeholder="이름" onChange={(e) => setEmpName(e.target.value)} />
+    //     <input type="number" placeholder="convSeq" onChange={(e) => setConvSeq(parseInt(e.target.value))} />
+    //     <input placeholder="생년월일 ex)19980123" onChange={(e) => setBirthDate(e.target.value)} />
+    //     <div>
+    //       <label>
+    //         <input 
+    //           type="radio" 
+    //           value="남성" 
+    //           checked={gender === "남성"} 
+    //           onChange={(e) => setGender(e.target.value)}
+    //         />
+    //         남성
+    //       </label>
+    //       <label>
+    //         <input 
+    //           type="radio" 
+    //           value="여성" 
+    //           checked={gender === "여성"} 
+    //           onChange={(e) => setGender(e.target.value)}
+    //         />
+    //         여성
+    //       </label>
+    //     </div>
+    //     <input type="tel" placeholder="전화번호" onChange={(e) => setPhoneNumber(e.target.value)} />
+    //     <input placeholder="고용일 ex)20230901" onChange={(e) => setHireDate(e.target.value)} />
+    //     <input placeholder="급여"  onChange={(e) => setSalary(parseInt(e.target.value))} />
+    //   </form>
+    //   </div>
+    //   <button onClick={ handleSubmit }>직원 추가하기</button>
+    // </div>
   );
 }
 
