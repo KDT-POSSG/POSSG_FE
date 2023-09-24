f10ffcde9d858510ef875479978e88f41ff8a902
yeonju <llyyjj292@gmail.com>
2023-09-25 01:08:28 +0900

    [Add] 발주 관련 페이지 컴포넌트화

---
+++ src/components/order/OrderDetailNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { orderState } from 'store/utils/function';
+
+function OrderDetailNav() {
+  return (
+    <div className='order-detail-middle'>
+      <div>
+        <div className='order-status order-status-1'>{orderState(1)}</div>
+
+        <div className='number-date'>
+          <div className='left'>
+            <span>발주번호</span>
+            &nbsp;&nbsp;&nbsp;202309051811
+          </div>
+          <div className='right'>
+            <span>발주날짜</span>
+            &nbsp;&nbsp;&nbsp;2023-09-05 00:00:00
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button className='order-cancel'>발주 취소</button>
+      </div>
+    </div>
+  )
+}
+
+export default OrderDetailNav;
