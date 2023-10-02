bd46eba3c8f7e27682079bf3a1b03f8b91ef2d9c
정재원 <gpffhwodnjs@gmail.com>
2023-10-02 22:24:28 +0900

    [Fix] 시재 페이지 분기처리, css 보완

--- src/components/payment/TosspayReceiptInfoModal.jsx
+++ src/components/payment/TosspayReceiptInfoModal.jsx
@@ -1,10 +1,12 @@
 
-function TosspayReceiptInfoModal() {
+function TosspayReceiptInfoModal({ receiptURL }) {
 
+      console.log("영수증",receiptURL);
+      
       return (
         <div className="cashpayreceipt">
           <iframe className="cashpayreceipt-iframe"
-            src="https://door.bootpay.co.kr/receipt/Q3lzS1BoejJHQWNTeWxERm5kVU1MV0FvSzVlZWNqUWJzZko4YWFpOUMyWm5v%0AZz09LS1rSm5xRldRWDByYnZYWnRmLS1ETFhMYWNlRVkzUnRiemdTeFlVUnhB%0APT0%3D%0A"
+            src={receiptURL.data.receipt_url}
             width="100%"
             height="600px">
           </iframe>
