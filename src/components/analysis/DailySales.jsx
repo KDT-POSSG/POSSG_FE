3c39fe431fc427094eb413dde7a8f309b271d63e
vvvmin <117338227+vvvmin@users.noreply.github.com>
2023-10-03 15:52:56 +0900

    [Add] 일별 매출 보고서 구현

--- src/components/analysis/DailySales.jsx
+++ src/components/analysis/DailySales.jsx
@@ -1,12 +1,46 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import ReactDatePicker from 'react-datepicker';
 
 function DailySales(){
     const [selectedDate, setSelectedDate] = useState(null);
+    const [salesData, setSalesData] = useState([]); // 매출 데이터 저장
+    const [expenseData, setExpenseData] = useState([]); // 지출 데이터 저장
+    const [profitData, setProfitData] = useState([]); // 순이익 데이터 저장
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
+
+    const onClick = async () => {   
+        try {
+            // API 호출 및 데이터 가져오기
+            // 예: const response = await fetch(`/api/sales?date=${selectedDate}`);
+            // const data = await response.json();
+            // axios.post("http://54.180.60.149:3000/dailySales")
+
+            // 가상의 데이터 (실제 데이터로 대체)
+            // const data = {
+            //     sales: [/* sales 데이터 배열 */],
+            //     expenses: [/* 지출 데이터 배열 */],
+            //     profit: [/* 순이익 데이터 배열 */],
+            // };
+
+            // setSalesData(data.sales);
+            // setExpenseData(data.expenses);
+            // setProfitData(data.profit);
+
+        } catch (error) {
+            console.error('데이터 가져오기 오류:', error);
+        }
+    }
+
+    // useEffect(() => {
+    //     if (selectedDate) {
+    //         fetchData();
+    //     }
+    // }, [selectedDate]);
+
     return(
         <div className="daily-content-wrap">
             <div className="daily-nav">
@@ -20,7 +54,7 @@ function DailySales(){
                         maxDate={new Date()} 
                     />
                     <div className="material-symbols-rounded">calendar_month</div>
-                    <button className="calendar-button" type="button">조회</button>
+                    <button className="calendar-button" type="button" onClick={onClick}>조회</button>
                 </div>
             </div>
         </div>
