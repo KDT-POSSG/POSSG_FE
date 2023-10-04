5cf9be366a53226adfbbd87d0faacb8e9a9c9826
yeonju <llyyjj292@gmail.com>
2023-10-04 10:35:41 +0900

    [Add] axios instance

--- src/store/apis/base.js
+++ src/store/apis/base.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 
 // baseurl
-// export const baseURL = "http://10.10.10.81:3000";
 export const baseURL = "http://54.180.60.149:3000";
 
-// 요청
+export const ACCESS_TOKEN = localStorage.getItem("accesstoken");
+// export const CONV_SEQ = localStorage.getItem("convSeq");
+export const CONV_SEQ = 1;
+
+// TOKEN X 
+export const nonTokenRequest = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// TOKEN O
 export const basicRequest = axios.create({
   baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
+    accessToken: `Bearer ${ACCESS_TOKEN}`,
+    Authorization: `Bearer ${ACCESS_TOKEN}`
   },
 });
--- src/store/apis/product.js
+++ src/store/apis/product.js
@@ -1,10 +1,8 @@
 import { basicRequest } from "./base";
 
-const convSeq = localStorage.getItem("convSeq");
-
 export const getProductList = async (keyword) => {
   const response = await basicRequest.get('/productList', {
-    params: keyword
+    params: keyword,
   });
   return response;
 }
