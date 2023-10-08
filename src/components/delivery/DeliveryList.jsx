4482092cfd03b9980585ce0d258889dc3c416079
yeonju <llyyjj292@gmail.com>
2023-10-08 12:11:54 +0900

    [Fix] 배달 시간 수정

--- src/components/delivery/DeliveryList.jsx
+++ src/components/delivery/DeliveryList.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { addComma } from 'store/utils/function';
 
 function DeliveryList() {
+
+  let delDate = "2023-09-13 14:42:14";
+
+  let month = delDate.slice(5, 7);
+  let day = delDate.slice(8, 10);
+  let time = delDate.slice(11, 16);
+
   return (
     <div className='delivery-list-container'>
       
       <div className='delivery-list-item'>
 
         <div className='item-top'>
-          <div>emart24</div>
-          <div>14:06</div>
+          <div className='convenience-name'>emart24</div>
+          <div className='delivery-time'>{month}월 {day}일 {time}</div>
         </div>
 
         <div className='item-middle'>
