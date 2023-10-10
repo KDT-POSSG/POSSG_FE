234abafa740e6d1bc16b87fc0efa84ef88be0e18
yeonju <llyyjj292@gmail.com>
2023-10-10 14:17:49 +0900

    [Add] 알람 API 테스트

--- src/components/ui/HeaderAlarm.jsx
+++ src/components/ui/HeaderAlarm.jsx
@@ -1,15 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Modal from './Modal';
+import { ACCESS_TOKEN, baseURL } from 'store/apis/base';
 
 function HeaderAlarm() {
 
+  const alarmStyle = {
+    content: {
+      width: '20rem',
+      height: '100vh',
+      minHeight: '100vh',
+      borderRadius: '0',
+      top: '0',
+      left: 'calc(100% - 20rem)',
+      transform: 'translate(0, 0)',
+      padding: '0',
+    },
+  };
+
+  const [alarmList, setAlarmList] = useState([]);
   const [isAlarm, setIsAlarm] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // useEffect(() => {
+
+  //   const eventSource = new EventSource(`${baseURL}/notifications`, {
+  //     headers: {
+  //       accessToken: `Bearer ${ACCESS_TOKEN}`, // 여기에 실제 토큰을 추가하세요.
+  //   }});
+
+  //   eventSource.onmessage = function(event) {
+  //     const product = JSON.parse(event.data);
+  //     console.log("event >> ", event);
+  //     console.log("event.data >> ", event.data);
+  //     console.log("product >> ", product);
+  //   };
+
+  //   eventSource.onerror = function(error) {
+  //     console.error("SSE Error:", error);
+  //   };
+    
+  // }, []);
+
+  const modalOpen = () => {
+    setIsModalOpen(true);
+  }
+
+  const modalClose = () => {
+    setIsModalOpen(false);
+  }
 
   return (
     <div className='header-alarm'>
-      <span className="material-symbols-rounded">
+      <span className="material-symbols-rounded menu-icon" onClick={modalOpen}>
         notifications
       </span>
       <div className={isAlarm ? 'alarm-dot' : ''}></div>
+
+      <Modal isOpen={isModalOpen} onClose={modalClose} style={alarmStyle}>
+        알람입니다
+      </Modal>
     </div>
   )
 }
