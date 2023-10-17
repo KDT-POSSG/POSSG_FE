96213d8fff666b8509b272c4181b3423bd7cfcbd
yeonju <llyyjj292@gmail.com>
2023-10-17 09:03:11 +0900

    [Add] 무한스크롤 구현 중

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useRef, useState } from 'react';
 import ProductNav from '../components/product/ProductNav';
 import ProductItem from '../components/product/ProductItem';
 import axios from 'axios';
-import { getProductList } from 'store/apis/product';
 import { baseURL } from 'store/apis/base';
 
 function ProductList() {
 
   const accesstoken = localStorage.getItem("accesstoken");
 
+  const [bottom, setBottom] = useState(null);
+	const bottomObserver = useRef(null);
+
   const [product, setProduct] = useState([]);
+
+  const [search, setSearch] = useState("");
   const [page, setPage] = useState(0);
+
   const [keyword, setKeyword] = useState({
     choice: "productName",
     pageNumber: page,
@@ -18,6 +23,47 @@ function ProductList() {
     search: null,
     sortOrder: "newest"
   });
+
+  const getProduct = async () => {
+
+    try {
+      const response = await axios.get(`${baseURL}/productList`, {
+        params: keyword,
+        headers: {
+          accessToken: `Bearer ${accesstoken}`
+        }
+      })
+      console.log(response.data);
+      return response.data;
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  const loadMoreData = () => {
+
+    const nextPage = page + 1;
+    setKeyword((prevKeyword) => ({
+      ...prevKeyword,
+      pageNumber: nextPage,
+    }));
+
+    axios
+      .get(`${baseURL}/productList`, {
+        params: keyword,
+        headers: {
+          accessToken: `Bearer ${accesstoken}`
+        }
+      })
+      .then((response) => {
+        setProduct((prevProduct) => [...prevProduct, ...response.data.ProductList]);
+        setPage(nextPage); // 페이지 번호 업데이트
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   useEffect(() => {
 
@@ -33,7 +79,8 @@ function ProductList() {
         .then((response) => {
           console.log(response.data);
           console.log(response.data.ProductList);
-          setProduct(response.data);
+          // setProduct(response.data);
+          setProduct((prevProduct) => [...prevProduct, ...response.data.ProductList]);
         })
         .catch((error) => {
           console.error(error);
@@ -44,6 +91,33 @@ function ProductList() {
     
   }, [keyword]);
 
+  
+
+  useEffect(() => {
+		const observer = new IntersectionObserver(
+			entries => {
+				if (entries[0].isIntersecting) {
+          loadMoreData();
+          console.log("1");
+				}
+			},
+			{ threshold: 0.25, rootMargin: '0px' },
+		);
+		bottomObserver.current = observer;
+	}, []);
+
+	useEffect(() => {
+		const observer = bottomObserver.current;
+		if (bottom) {
+			observer.observe(bottom);
+		}
+		return () => {
+			if (bottom) {
+				observer.unobserve(bottom);
+			}
+		};
+	}, [bottom]);
+
   return (
     <div className='product-page'>
 
@@ -53,9 +127,9 @@ function ProductList() {
         <div>
           <ProductNav keyword={keyword} setKeyword={setKeyword} />
         </div>
-
+        
         {
-          !product.ProductList || product.ProductList.length === 0 ? 
+          !product || product.length === 0 ? 
           (
             <div className='product-noitem'>
               <span className='tossface'>📦</span>
@@ -68,12 +142,15 @@ function ProductList() {
 
         <div className='product-item-container'>
           {
-            product.ProductList && product.ProductList.map((item) => (
+            product && product.map((item) => (
               <ProductItem key={item.productSeq} product={item} />
             ))
           }
         </div>
       </div>
+
+      <div ref={setBottom} style={{ height: "300px", backgroundColor: "skyblue" }}>loading...</div>
+      {/* <div ref={setBottom}>loading...</div> */}
 
     </div>
   )
