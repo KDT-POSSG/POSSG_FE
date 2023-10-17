9d6593be48a685c87a313181f813a464b73b0562
vvvmin <117338227+vvvmin@users.noreply.github.com>
2023-10-18 00:57:09 +0900

    [Add] 매출 페이지 구현

--- src/components/analysis/DailySales.jsx
+++ src/components/analysis/DailySales.jsx
@@ -1,28 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import MyChart from '../../store/utils/MyChart';
 import { baseURL } from 'store/apis/base';
 import { addComma } from 'store/utils/function';
 import Calendar from './Calendar';
+import toast from 'react-hot-toast';
+import MyPieChart from 'store/utils/MyPieChart';
 
 function DailySales(){
     const accesstoken = localStorage.getItem("accesstoken");
 
     const [selectedDate, setSelectedDate] = useState(null);
-    const [salesData, setSalesData] = useState([]); 
+    const [salesData, setSalesData] = useState({}); 
     const [data, setData] = useState(false);
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
 
-    const chartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true,
-                max: 100000, // 임시로 설정
-            },
-        },
+    const getIncreaseRateColor = () => {
+        if (salesData.increaseRate > 0) {
+            return "positive-increase";
+        } else if (salesData.increaseRate === 0) {
+            return "zero-increase";
+        } else {
+            return "negative-increase";
+        }
     };
 
     const fetchData = async (selectedDate) => {   
@@ -38,18 +40,19 @@ function DailySales(){
                     accessToken: `Bearer ${accesstoken}`,
                 },
             })
-            // console.log("DailySales res >>> ", res);
+            console.log("DailySales res >>> ", res);
             const resData = res.data;
             console.log("resData >>> ", resData)
             if(resData){
                 setSalesData(resData);
                 setData(true);
-            }else{
+            }
+            else{
                 setSalesData(0)
                 setData(true);
-            }
-            
+            }     
         } catch (err) {
+            toast.error("해당 날짜에는 조회할 자료가 없습니다");
             console.error('try-catch 오류:', err);
             setData(false);
         }
@@ -80,12 +83,77 @@ function DailySales(){
                 <div className="sales-data-container">
                     <div className="sales-data">
                         <div className="sales-data-title">총 매출</div>
-                        <div className="sales-data-amount">{addComma(salesData)}</div>
+                        <div className="sales-data-amount">{addComma(salesData.totalPrice)}원</div>
+                            {salesData.increaseRate !== undefined && (
+                                <p className={`sales-data-increaseRate ${getIncreaseRateColor()}`}>
+                                    {salesData.increaseRate > 0
+                                    ? `↑${salesData.increaseRate}%`
+                                    : salesData.increaseRate === 0
+                                    ? `${salesData.increaseRate}%`
+                                    : `↓${salesData.increaseRate}%`}
+                                </p>
+                            )}
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">결제 건수</div>
+                        <div className="sales-data-amount">{addComma(salesData.totalCount)}건</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">할인 금액</div>
+                        <div className="sales-data-amount">{addComma(salesData.discountPrice)}원</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">할인 건수</div>
+                        <div className="sales-data-amount">{addComma(salesData.discountCount)}건</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">환불 금액</div>
+                        <div className="sales-data-amount">{addComma(salesData.refundPrice)}원</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">환불 건수</div>
+                        <div className="sales-data-amount">{addComma(salesData.refundCount)}건</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">총 비용</div>
+                        <div className="sales-data-amount">{addComma(salesData.totalLoss)}원</div>
+                    </div>
+                    <div className="sales-data">
+                        <div className="sales-data-title">순 이익</div>
+                        <div className="sales-data-amount">{addComma(salesData.profit)}원</div>
                     </div>
                 </div>
-                <div className="sales-chart"> 
-                    <MyChart data={[salesData]} labels={["매출"]} chartOptions={chartOptions} />
-                </div>
+
+                {salesData.totalPrice === 0 && salesData.totalLoss === 0 && salesData.profit === 0 ? (
+                    <div className="sales-no-datas">
+                        <p className="select-no-text">저장된 데이터가 없습니다</p>&nbsp;&nbsp;&nbsp;
+                        <span class="material-symbols-rounded chart-icon">bar_chart_4_bars</span>       
+                    </div>
+                ) : (
+                    <div className="sales-chart-datas"> 
+                        <div className="sales-chart">
+                            <MyPieChart
+                                data={[salesData.totalPrice, salesData.totalLoss, salesData.profit]}
+                                labels={["총 매출", "총 비용", "순 이익"]}
+                                chartOptions={{}}
+                            />
+                        </div>
+                        <div className="sales-datas-container">
+                            <div className="sales-datas">
+                                <div className="sales-datas-title">총 매출</div>
+                                <div className="sales-datas-amount">{addComma(salesData.totalPrice)}원</div>
+                            </div>
+                            <div className="sales-datas">
+                                <div className="sales-datas-title">총 비용</div>
+                                <div className="sales-datas-amount">{addComma(salesData.totalLoss)}원</div>
+                            </div>
+                            <div className="sales-datas">
+                                <div className="sales-datas-title">순 이익</div>
+                                <div className="sales-datas-amount">{addComma(salesData.profit)}원</div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
             ) : (
                 <div className="select-date-message">
