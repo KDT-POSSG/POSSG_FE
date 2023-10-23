df9e0a66715dcfe1ba15996d8a2386d4878437e4
vvvmin <117338227+vvvmin@users.noreply.github.com>
2023-10-23 11:04:46 +0900

    [Add] 월세 및 관리비 페이지 수정

--- src/components/analysis/Cost.jsx
+++ src/components/analysis/Cost.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Cost(){
+    const navi = useNavigate();
     return(
         <div className="cost-content-wrap">
             <div className="cost-content">
-                <Link to="/addCost"><div className="cost">월세 및 관리비 입력</div></Link>
-                <Link to="/updateCost"><div className="cost">월세 및 관리비 수정</div></Link>
+                <div className="list-btn">
+                    <button onClick={() => navi("/analysis")}>목록</button>
+                </div>
+                <div className="link-content">
+                    <Link to="/addCost"><div className="cost">월세 및 관리비 입력</div></Link>
+                    <Link to="/updateCost"><div className="cost">월세 및 관리비 수정</div></Link>
+                </div>
+
             </div>
         </div>
     )
